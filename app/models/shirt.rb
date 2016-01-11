5edc88daa82c54dd8774a610e179d8f322963fd4
wpaszczu <wpaszczu@gmail.com>
2016-01-11 17:13:21 +0100

    user to shirts but working

--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -1,4 +1,5 @@
 class Shirt < ActiveRecord::Base
 
-  belongs_to: user
+  belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109175941) do
+ActiveRecord::Schema.define(version: 20160111155553) do
 
   create_table "shirts", force: :cascade do |t|
     t.string   "inscription"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160109175941) do
     t.boolean  "availability", default: true
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
